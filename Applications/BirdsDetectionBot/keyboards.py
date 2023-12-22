a1486e2308512149c8367664ee0b823bef78ddcc
NazarovMichail <126945101+NazarovMichail@users.noreply.github.com>
2023-12-22 22:39:32 +0300

    m main.py comments

--- Applications/BirdsDetectionBot/keyboards.py
+++ Applications/BirdsDetectionBot/keyboards.py
@@ -1,32 +1,40 @@
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup
 
-
+# Клавиатура главного меню
 kbrd_menu = ReplyKeyboardMarkup(resize_keyboard=True)
 info_btn = KeyboardButton('О проекте')
 kbrd_menu.add(info_btn)
 
+
+# Кнопка под фото голубя
 inline_dove = InlineKeyboardMarkup(row_width=3)
 wiki_dove = InlineKeyboardButton(text='Узнать о голубе', url='https://ru.wikipedia.org/wiki/Голуби')
 inline_dove.add(wiki_dove)
 
+
+# Кнопка под фото синицы
 inline_bluetit = InlineKeyboardMarkup(row_width=3)
 wiki_bluetit = InlineKeyboardButton(text='Узнать о синице', url='https://ru.wikipedia.org/wiki/Большая_синица')
 inline_bluetit.add(wiki_bluetit)
 
 
+# Кнопка под фото дрозда
 inline_thrush = InlineKeyboardMarkup(row_width=3)
 wiki_thrush = InlineKeyboardButton(text='Узнать о дрозде', url='https://en.wikipedia.org/wiki/Thrush_(bird)')
 inline_thrush.add(wiki_thrush)
 
 
+# Кнопка под фото воробья
 inline_sparrow = InlineKeyboardMarkup(row_width=3)
 wiki_sparrow = InlineKeyboardButton(text='Узнать о воробье', url='https://en.wikipedia.org/wiki/House_sparrow')
 inline_sparrow.add(wiki_sparrow)
 
+# Кнопка под фото дятла
 inline_woodpecker = InlineKeyboardMarkup(row_width=3)
 wiki_woodpecker = InlineKeyboardButton(text='Узнать о дятле', url='https://en.wikipedia.org/wiki/Woodpecker')
 inline_woodpecker.add(wiki_woodpecker)
 
+# Кнопка под фото кардинала
 inline_cardinal = InlineKeyboardMarkup(row_width=10)
 wiki_cardinal = InlineKeyboardButton(text='Узнать о красном кардинале', url='https://ru.wikipedia.org/wiki/Красный_кардинал')
 inline_cardinal.add(wiki_cardinal)
--- Applications/BirdsDetectionBot/main.py
+++ Applications/BirdsDetectionBot/main.py
@@ -6,14 +6,17 @@ from keyboards import kbrd_menu, inline_dove, inline_bluetit, inline_thrush, inl
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 
+# Подключение модели по API Roboflow
 from roboflow import Roboflow
 rf = Roboflow(api_key="jyxqZLcJ27mR9wFxBpUJ")
 project = rf.workspace().project("birds-detection-fld02")
 model = project.version(2).model
 
-bot = Bot(TOKEN)
-storage = MemoryStorage()
-dispatcher = Dispatcher(bot, storage=storage)
+
+bot = Bot(TOKEN)  # Создание экземпляра бота с токеном
+storage = MemoryStorage()  # Создание хранилища для буфера обмена
+dispatcher = Dispatcher(bot, storage=storage)  # Создание обработчика сообщений
+
 
 PROJECT_INFO = """
 Бот разработан для детекции птиц по фото.
@@ -44,53 +47,52 @@ START_TEXT = """
 <b>Загрузите фотографию птиц 🐦‍⬛️</b>
 """
 
-
-class InputState(StatesGroup):  
-# Класс для считывания текущего состояния
-    input_state = State()
-
-
+# Функция при запуске бота
 async def bot_startup(dispatcher):
     print('Bot is running ...')
 
 
+# Функция при выходе из бота
 async def bot_shutup(dispatcher):
     # При выходе пишется сообщение
     print('Bot has gone ...')
 
 
-# Вывод информации
+# Функция при получении сообщения "О проекте"
 @dispatcher.message_handler(Text(equals='О проекте'))
 async def proj_info(message: types.Message):
     await message.answer(text=f' {PROJECT_INFO} ',
-                        parse_mode= "HTML")
-    await message.delete()
+                        parse_mode= "HTML")  # Выводится текст "PROJECT_INFO"
+    await message.delete()  # Входящее сообщение удаляется
 
-# Вывод клавиатуры меню
+
+# Функция при получении команды "/start"
 @dispatcher.message_handler(commands=['start'])
 async def get_keyboard(message: types.Message):
-    await message.answer(text=START_TEXT, parse_mode='HTML', reply_markup=kbrd_menu)
-    await bot.send_animation(chat_id=message.from_user.id, animation=open('img/presentation.gif', 'rb'))
+    await message.answer(text=START_TEXT, parse_mode='HTML', reply_markup=kbrd_menu)  # Выводится текст "START_TEXT" и клавиатура главного меню
+    await bot.send_animation(chat_id=message.from_user.id, animation=open('img/presentation.gif', 'rb'))  # Выводится gif-презентация
     await message.delete()
 
 
+# Функция при получении входящей фотографии
 @dispatcher.message_handler(content_types=['photo'])
 async def get_photo(message: types.Message):   
-    await message.photo[-1].download('model/file.jpg')
-    prediction = model.predict('model/file.jpg', confidence=40, overlap=30).json()
-    print(prediction)
-    model.predict("model/file.jpg", confidence=40, overlap=30).save("model/prediction.jpg")
+    await message.photo[-1].download('model/file.jpg')  #  Фото сохраняется на сервер
+    prediction = model.predict('model/file.jpg', confidence=40, overlap=30).json()  # Скачанное фото помещается в модель МО и возвращается json с детектированными птицами
+    model.predict("model/file.jpg", confidence=40, overlap=30).save("model/prediction.jpg") # Фото с детектированными птицами сохраняется на сервер
     await message.delete()
-    await bot.send_photo(chat_id=message.from_user.id, photo=open('model/prediction.jpg', 'rb'))
-    class_list = []
+    await bot.send_photo(chat_id=message.from_user.id, photo=open('model/prediction.jpg', 'rb'))  # Фото с детектированными птицами отправляется в бот
+    
+    class_list = []  # Список для всех детектированных птиц
     try:
         if prediction['predictions'] == []:
-                await message.answer(text="""На изображении нет птиц 🤷... Ведь так? 🫣""")
-        for pred in prediction['predictions']:
-            if pred['class'] not in class_list:
-                if pred['class'] == "pigeon":
+                await message.answer(text="""На изображении нет птиц 🤷... Ведь так? 🫣""")  # Если птицы не детектированы, выводится сообщение
+        for pred in prediction['predictions']:  # Проходим по списку со всеми детектированными птицами
+            if pred['class'] not in class_list:  # Если птица еще не возвращалась в боте
+                if pred['class'] == "pigeon":  # Если детектированная птица - голубь
+                    # В бот возвращается фото птицы из сервера и кнопка " Узнать о голубе"
                     await bot.send_photo(chat_id=message.from_user.id, photo=open('img/pigeon.jpg', 'rb'), caption='Голубь', reply_markup=inline_dove)
-                    class_list.append(pred['class'])
+                    class_list.append(pred['class'])  # Название детектированной птицы добавляется в список всех детектированных птиц
                 if pred['class'] == "tit":
                     await bot.send_photo(chat_id=message.from_user.id, photo=open('img/titmouse.jpg', 'rb'), caption='Синица', reply_markup=inline_bluetit)
                     class_list.append(pred['class'])
@@ -110,14 +112,13 @@ async def get_photo(message: types.Message):
                 if pred['class'] == "cardinal":
                     await bot.send_photo(chat_id=message.from_user.id, photo=open('img/cardinal.jpg', 'rb'), caption='Красный кардинал', reply_markup=inline_cardinal)
                     class_list.append(pred['class'])
-            
-                
     except IndexError:
         await message.answer(text="""На изображении нет птиц 🤷... Ведь так? 🫣""")
 
 
+# Запуск бота
 if __name__ == '__main__':
     executor.start_polling(dispatcher=dispatcher,
                         on_startup=bot_startup,
                         skip_updates=True,
-                        on_shutdown=bot_shutup)
+                        on_shutdown=bot_shutup)  # Активация функция при запуске и выходе из бота
